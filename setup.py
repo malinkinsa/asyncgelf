ada4559389e6033bfc61d92c283d337d26e082e2
Sergey Malinkin <malinkinsa@yandex.ru>
2022-04-07 18:34:38 +0300

    download url to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='Sergey Malinkin',
     author_email='malinkinsa@yandex.ru',
     url='https://github.com/malinkinsa/asyncgelf',
+    download_url='https://github.com/malinkinsa/asyncgelf/archive/refs/tags/0.0.2.tar.gz',
     description='Async python logging handlers that send messages in the Graylog Extended Log Format (GELF).',
     long_description=open('README.md').read(),
     license='MIT',
