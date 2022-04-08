85d586ea4130188f6a2d3e977116aa1bc3772205
Sergey Malinkin <malinkinsa@yandex.ru>
2022-04-08 23:32:20 +0300

    Comments to code

--- asyncgelf/asyncgelf.py
+++ asyncgelf/asyncgelf.py
@@ -15,6 +15,13 @@ class GelfBase(object):
             level: Optional[str] = 1,
             scheme: Optional[str] = 'http'
     ):
+        """
+        :param host: graylog server address
+        :param port: graylog input port
+        :param gelf_version: GELF spec version
+        :param level: the level equal to the standard syslog levels
+        :param scheme: HTTP Scheme for GELF HTTP input only
+        """
 
         self.host = host
         self.port = port
@@ -24,6 +31,11 @@ class GelfBase(object):
         self.scheme = scheme
 
     def make(self, message):
+        """
+        Transforms each message into GELF
+        :param message: input message
+        :return: a dictionary representing a GELF log
+        """
         gelf_message = {
             'version': self.gelf_version,
             'host': self.domain,
@@ -35,20 +47,31 @@ class GelfBase(object):
 
 class GelfTcp(GelfBase):
     async def tcp_handler(self, massage):
+        """
+        tcp handler for send logs to Graylog Input with type: gelf tcp
+        :param massage: input message
+        :return:
+        """
         gelf_message = GelfBase.make(self, massage)
-
+        """ Transforming GELF dictionary into bytes """
         bytes_msg = json.dumps(gelf_message).encode('utf-8')
 
         stream_reader, stream_writer = await asyncio.open_connection(
             self.host, self.port
         )
 
+        """ if you send the message over tcp, it should always be null terminated or the input will reject it """
         stream_writer.write(bytes_msg + b'\x00')
         stream_writer.close()
 
 
 class GelfHttp(GelfBase):
     async def http_handler(self, message):
+        """
+        http handler for send logs to Graylog Input with type: gelf http
+        :param message: input message
+        :return: http status code
+        """
         header = {
             'Content-Type': 'application/json',
             'Content-Encoding': 'gzip,deflate',
