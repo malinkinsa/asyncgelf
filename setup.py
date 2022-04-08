c1b0d6e13c86b8ea1327819107a4237809ece6e8
Sergey Malinkin <malinkinsa@yandex.ru>
2022-04-08 23:57:48 +0300

    Fix

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     download_url='https://github.com/malinkinsa/asyncgelf/archive/refs/tags/0.1.0.tar.gz',
     description='Async python logging handlers that send messages in the Graylog Extended Log Format (GELF).',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
     keywords='gelf logging graylog graylog2 tcp',
     packages=find_packages(),
