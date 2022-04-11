b87c5541d40198482c86bb40bdbce803d58068bd
Sergey Malinkin <malinkinsa@yandex.ru>
2022-04-11 18:49:14 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='asyncgelf',
-    version='0.1.0',
+    version='0.1.1',
     author='Sergey Malinkin',
     author_email='malinkinsa@yandex.ru',
     url='https://github.com/malinkinsa/asyncgelf',
-    download_url='https://github.com/malinkinsa/asyncgelf/archive/refs/tags/0.1.0.tar.gz',
+    download_url='https://github.com/malinkinsa/asyncgelf/archive/refs/tags/0.1.1.tar.gz',
     description='Async python logging handlers that send messages in the Graylog Extended Log Format (GELF).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
